23d145e11a3edfe8edc7525a902bb464ced4647e
BowenTang1996 <50333602+BowenTang1996@users.noreply.github.com>
2019-05-27 15:56:58 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overal function is to cache the results of caiculating inverse matrix. 
+## For the first time of calculating a inverse matrix, R will cache the result. 
+## Then, when we want to use the inverse matrix again, this function will get the values 
+## from cache instead of calculating it again. 
 
-## Write a short comment describing this function
+
+## Creat set, get, setinverse, and get inverse functions. Using the scooping rules and the ervironment 
+## of each function to cache the original matrix and inverse matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
+        if(!is.matrix(x)) {message("what you enter is not a matrix")} # to warn the input data type 
         m <- NULL
-        set <- function(y) {
+        set <- function(y) {    
                 x <<- y
                 m <<- NULL
-        }
+        } ## modify 'x' and 'm' in the parent enviroment,
+          ## 'set' fucntion can be used to input a new matrix
         get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        setinverse <- function(inverse) m <<- inverse ## to modify 'm' in the parent enviroment
+        getinverse <- function() m 
         list(set = set, get = get,
              setinverse = setinverse,
-             getinverse = getinverse)
+             getinverse = getinverse) ## to enable us call each fucntion using $
 
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is to get the cached values from the 'makeCacheMatrix' marix 
+## and check if having already beening calculated
+## if yes, just get and display the value from cache 
+## if no, calculate the inverse and cache it in 'makeCacheMatrix' environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        m <- x$getinverse() #get the cached value and make sure that the x must go through makeCacheMatrix 
+        if(is.null(m)) {
+                data <- x$get() # to get the data from 'makeCacheMatrix' environment 
+                m <- solve(data, ...)
+                x$setinverse(m) # return the calculation result to 
+                                # 'makeCacheMatrix' environment and cache it
+                m
+        } else {
+               message("getting cached data")
+        return(m) 
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
 }
 
